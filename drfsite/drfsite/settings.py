b02ee8ac92253d09a7e772900e29e023483d0d7a
Rostyslav Bysko <brn.h74@gmail.com>
2025-01-29 21:21:03 +0200

    add spectaculus

--- drfsite/drfsite/settings.py
+++ drfsite/drfsite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "women.apps.WomenConfig",
     "rest_framework",
+    'drf_spectacular',
 
 ]
 
@@ -124,3 +125,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+REST_FRAMEWORK = {
+'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,45 +1,23 @@
 from django.shortcuts import render
 from django.forms.models import model_to_dict
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Women
 from .serializers import WomenSerializer
 from rest_framework.views import APIView, Response
 # Create your views here.
 
+class WomenViewSet(viewsets.ModelViewSet):
+     queryset = Women.objects.all()
+     serializer_class = WomenSerializer
+# class WomenAPIList(generics.ListCreateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
-class WomenAPIView(APIView):
-    def get(self, request):
-        w = Women.objects.all()
-        return Response({'posts': WomenSerializer(w, many=True).data})
+# class WomenAPIUpdate(generics.UpdateAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
-# class WomenAPIView(APIView):
-#     def get(self, request):
-#         w = Women.objects.all()
-#         return Response({"posts": WomenSerializer(w, many=True).data})
+# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
 
-    # def post(self, request):
-    #     post_new = Women.objects.create(
-    #         title=request.data['title'],
-    #         content=request.data['content'],
-    #         cat_id=request.data['cat_id'])
-    #     return Response({'posts': WomenSerializer(post_new, many=True).data})
-
-    def post(self, request):
-        serializer = WomenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response({'post': serializer.data})
-
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method PUT not allowed"})
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response({"error": "Object does not exist"})
-        serializer = WomenSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"post": serializer.data})
