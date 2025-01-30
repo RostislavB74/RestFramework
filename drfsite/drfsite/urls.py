dbeaf6a5dc77d1a04c885862dd8994ef0b5e0d8a
Rostyslav Bysko <132708200+RostislavB74@users.noreply.github.com>
2025-01-30 22:04:05 +0200

    routers

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -15,11 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from women.views import WomenAPIView
+from django.urls import path, include
+from women.views import WomenViewSet
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register(r'women', WomenViewSet, basename='women')
+print(router.urls)
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/womenlist/", WomenAPIView.as_view()),
-    path("api/v1/womenlist/<int:pk>/", WomenAPIView.as_view()),
+    path("api/v1/", include(router.urls)),
+
+    # path("api/v1/womenlist/", WomenAPIView.as_view()),
+    # path("api/v1/womenlist/<int:pk>/", WomenAPIView.as_view()),
 ]
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,45 +1,55 @@
 from django.shortcuts import render
 from django.forms.models import model_to_dict
-from rest_framework import generics
-from .models import Women
+from rest_framework import generics, viewsets
+from .models import Women, Category
 from .serializers import WomenSerializer
 from rest_framework.views import APIView, Response
+from rest_framework.decorators import action
 # Create your views here.
 
 
-class WomenAPIView(APIView):
-    def get(self, request):
-        w = Women.objects.all()
-        return Response({'posts': WomenSerializer(w, many=True).data})
+class WomenViewSet(viewsets.ModelViewSet):
+    # queryset = Women.objects.all()
+    serializer_class = WomenSerializer
 
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        if not pk:
+            return Women.objects.all()[:3]
+        return Women.objects.filter(pk=pk)
+
+    @action(methods=['get'], detail=True)
+    def category(self, request, pk=None):
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': [cats.name]})
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         w = Women.objects.all()
+#         return Response({'posts': WomenSerializer(w, many=True).data})
 # class WomenAPIView(APIView):
 #     def get(self, request):
 #         w = Women.objects.all()
 #         return Response({"posts": WomenSerializer(w, many=True).data})
-
-    # def post(self, request):
-    #     post_new = Women.objects.create(
-    #         title=request.data['title'],
-    #         content=request.data['content'],
-    #         cat_id=request.data['cat_id'])
-    #     return Response({'posts': WomenSerializer(post_new, many=True).data})
-
-    def post(self, request):
-        serializer = WomenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response({'post': serializer.data})
-
-    def put(self, request, *args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response({"error": "Method PUT not allowed"})
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response({"error": "Object does not exist"})
-        serializer = WomenSerializer(data=request.data, instance=instance)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({"post": serializer.data})
+# def post(self, request):
+#     post_new = Women.objects.create(
+#         title=request.data['title'],
+#         content=request.data['content'],
+#         cat_id=request.data['cat_id'])
+#     return Response({'posts': WomenSerializer(post_new, many=True).data})
+#    def post(self, request):
+#         serializer = WomenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'post': serializer.data})
+#     def put(self, request, *args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response({"error": "Method PUT not allowed"})
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#         except:
+#             return Response({"error": "Object does not exist"})
+#         serializer = WomenSerializer(data=request.data, instance=instance)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({"post": serializer.data})
